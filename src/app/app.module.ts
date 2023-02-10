2ce326dfc94d6dc2bc0b7d7cf7bd73e718157e82
Brandon Emanuel Gonzalez Morales <brandonnemmanuel@hotmail.com>
2023-02-09 18:25:50 -0600

    Login con Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-firebase-auth-37233',
+    appId: '1:1008776354381:web:a07bdd7fcf98deffe2da04',
+    storageBucket: 'angular-firebase-auth-37233.appspot.com',
+    apiKey: 'AIzaSyCME_FjUz67hkf0_XbLr2PWYJVaI6UX_IE',
+    authDomain: 'angular-firebase-auth-37233.firebaseapp.com',
+    messagingSenderId: '1008776354381',
+  },};
